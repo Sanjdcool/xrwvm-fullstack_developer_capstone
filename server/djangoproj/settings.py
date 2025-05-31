4be7b9e5dcda367fc625f2bad268b8de40d21053
sanjdcool <sanj.iimi@gmail.com>
2025-05-31 07:29:16 -0400

    Commit

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,11 +31,11 @@ DEBUG = True
 # CSRF_TRUSTED_ORIGINS=['https://sanjaygupta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 ALLOWED_HOSTS = [
     'localhost',
-    'sanjaygupta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'sanjaygupta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://sanjaygupta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://sanjaygupta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 USE_X_FORWARDED_HOST = True
